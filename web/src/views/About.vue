4714a25bb4ebe6747a0179af427f6f4dc0cf7117
Neil Poulin <neil.j.poulin@gmail.com>
2020-03-18 21:53:53 -0600

    update configs

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Fishbowl</h1>
+    <p>I assume you hot reload</p>
   </div>
 </template>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="/images/fishbowl.png" />
+    <HelloWorld msg="Welcome to Fishbowl" />
   </div>
 </template>
 
